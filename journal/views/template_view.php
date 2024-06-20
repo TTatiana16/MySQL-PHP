525b2081bf33216d7ff2ddac6587095869b67893
TTatiana16 <104998693+TTatiana16@users.noreply.github.com>
2024-06-20 13:28:11 +0300

    Add files via upload

---
+++ journal/views/template_view.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<title>Главная</title>
+	<link href="style.css" rel='stylesheet'>
+</head>
+<body>
+	<?php include 'views/'.$content_view; ?>
+</body>
+</html>
